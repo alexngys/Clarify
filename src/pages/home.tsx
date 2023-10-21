067a97176bd09673e38dd974c106f25cbcdd2a22
Alex Ng <alexanderngyewshen@gmail.com>
2023-10-21 11:59:37 +0100

    [FEAT] Add Flowbire react component library

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { Button } from "flowbite-react";
 function Home() {
   return (
     <div>
       <h1>Welcome to my website!</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
