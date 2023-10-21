71e512e6d846149dbbbc79d1559190359407faa0
Alex Ng <alexanderngyewshen@gmail.com>
2023-10-21 18:07:19 +0100

    [FIX] Ui fixes

--- src/pages/SolidtyConvert.tsx
+++ src/pages/SolidtyConvert.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "flowbite-react";
 import { useParams } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import { Web3Provider } from "@ethersproject/providers";
-
 
 // TypeScript type assertion
 declare global {
@@ -27,41 +26,45 @@ function SolidtyConvert() {
 
         // Request account access
         const signer = provider.getSigner();
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
         const account = accounts[0];
         console.log("Connected to account:", account);
 
         // Here you would add the logic to deploy the contract using ethers.js
         // ...
-
       } catch (error) {
         console.error("User rejected access or there was an error:", error);
       }
     } else {
-      alert('Please install MetaMask!');
+      alert("Please install MetaMask!");
     }
-  }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-      console.log('Fetching data...');
+      console.log("Fetching data...");
       try {
         // Make POST request to backend
-        const response = await axios.post('http://localhost:3001/convertToSolidity', {
-          contractId: clarityAddress
-        });
+        const response = await axios.post(
+          "http://localhost:3001/convertToSolidity",
+          {
+            contractId: clarityAddress,
+          }
+        );
         // Assume the response data structure is as follows:
         // { sourceCodeData: { source: '...' }, gptResponse: { choices: [{ text: '...' }] } }
         // Update state with data from backend
         setCodeData(response.data.sourceCodeData.source);
-        setExplanation(response.data.solidity[0].message.content); 
-        setSolidityCode(response.data.solidity[1].message.content)
+        setSolidityCode(response.data.solidityCode);
+        setExplanation(response.data.explanation);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
     fetchData();
-  }, [clarityAddress]); 
+  }, []);
 
   return (
     <div>
@@ -75,28 +78,35 @@ function SolidtyConvert() {
               <h2 className="text-xl font-semibold mb-4">
                 Clarity Smart Contract Code
               </h2>
-              <pre>
-                <code className="language-clarity">
-                  {codeData || "Loading code..."}
-                </code>
+              <pre className="overflow-auto">
+                {codeData || "Loading code..."}
               </pre>
             </div>
             <div className="bg-white p-4 rounded shadow-md">
               <h2 className="text-xl font-semibold mb-4">
                 Solidity Smart Contract Code
               </h2>
-              <pre>
-                <code className="language-solidity">
-                  { solidityCode || "Loading explanation..."}
-                </code>
+              <pre className="overflow-auto">
+                {solidityCode || "Loading code..."}
               </pre>
             </div>
           </div>
-          <Button 
-    onClick={handleDeploy}
-    className="deploy-button font-bold mt-2 px-6 py-2 bg-amber-500 text-white rounded ml-[70%]">
-    DEPLOY
-</Button>
+        </div>
+        <div className=" my-4 mx-5">
+          <div className="bg-white p-4 rounded shadow-md ">
+            <div className="flex justify-between">
+              <h2 className="text-2xl font-semibold mb-4 ">Explanation</h2>
+              <Button
+                onClick={handleDeploy}
+                className="deploy-button font-bold mt-2 px-6 py-2 bg-amber-500 text-white rounded "
+              >
+                DEPLOY
+              </Button>
+            </div>
+            <p className="break-words whitespace-pre-wrap ">
+              {explanation || "Loading explanation..."}
+            </p>
+          </div>
         </div>
       </div>
     </div>
