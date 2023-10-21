c94eb396d3ccf57c701b97753635cff37db6d0ea
Alex Ng <alexanderngyewshen@gmail.com>
2023-10-21 11:42:48 +0100

    [FEAT] Add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to my website!</h1>
+    </div>
+  );
+}
+
+export default Home;
