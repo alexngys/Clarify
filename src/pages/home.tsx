79f77497f01094b493b3e6f2c33de314cfa0b3d4
arthur-wronski <arthur.wronski.21@ucl.ac.uk>
2023-10-21 13:51:08 +0100

    frontend input

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,17 +3,17 @@ import { Tabs } from "flowbite-react";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      {/* Large Bold Centered Title */}
-      <h1 className="text-4xl font-bold mb-8">Clarify</h1>
+    <div className="flex flex-col items-center justify-start h-screen text-center bg-gradient-to-r from-blue-200 to-blue-300">
+      {/* Even Larger Bold Centered Title */}
+      <h1 className="text-8xl font-bold mb-8 mt-8 text-white">CLARIFY</h1>
 
-      <Tabs.Group aria-label="Clarify tabs" style="default">
+      <Tabs.Group aria-label="Clarify tabs" style="default" className="justify-center">
         {/* Clarity Tab */}
         <Tabs.Item
           active
           title="Clarity"
         >
-          <p>
+          <p className="mx-auto">
             This is
             <span className="font-medium text-gray-800 dark:text-white">
               Clarity tab's associated content
@@ -22,13 +22,20 @@ function Home() {
             Clicking the other tab will toggle the visibility of this one. The tab JavaScript swaps classes to
             control the content visibility and styling.
           </p>
+          {/* Input box beneath Clarity Tab */}
+          <input
+            placeholder="Enter your address here..."
+            className="mt-4 px-4 py-3 border rounded placeholder-gray-500 text-lg w-2/4 h-12 resize-y"
+          ></input>
+          {/* Submit button below the input box */}
+          <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded">Submit</button>
         </Tabs.Item>
 
         {/* Solidity Tab */}
         <Tabs.Item
           title="Solidity"
         >
-          <p>
+          <p className="mx-auto">
             This is
             <span className="font-medium text-gray-800 dark:text-white">
               Solidity tab's associated content
@@ -37,6 +44,13 @@ function Home() {
             Clicking the other tab will toggle the visibility of this one. The tab JavaScript swaps classes to
             control the content visibility and styling.
           </p>
+          {/* Input box beneath Solidity Tab */}
+          <input
+            placeholder="Enter your address here..."
+            className="mt-4 px-4 py-3 border rounded placeholder-gray-500 text-lg w-3/4 h-12 resize-y"
+          ></input>
+          {/* Submit button below the input box */}
+          <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded">Submit</button>
         </Tabs.Item>
       </Tabs.Group>
     </div>
