3e6a9d7342450a680bc579b73a28a201ff76255d
Alex Ng <alexanderngyewshen@gmail.com>
2023-10-21 12:36:17 +0100

    [FEAT] Added clarity address page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import ClarityConvert from "./pages/ClarityConvert";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/ClarityConvert/:clarityAddress"
+          element={<ClarityConvert />}
+        />
       </Routes>
     </div>
   );
--- src/pages/ClarityConvert.tsx
+++ src/pages/ClarityConvert.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Button } from "flowbite-react";
-function Home() {
+import { useParams } from "react-router-dom";
+
+function ClarityConvert() {
+  let { clarityAddress } = useParams();
   return (
     <div>
       <h1 className="">Welcome to my website!</h1>
+      <h2>{clarityAddress}</h2>
       <Button>Click me</Button>
     </div>
   );
 }
 
-export default Home;
+export default ClarityConvert;
