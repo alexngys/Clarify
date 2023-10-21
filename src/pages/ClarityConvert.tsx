3c00cf14ed5d6dc47a5488ef8579ecaab02e65e4
Alex Ng <alexanderngyewshen@gmail.com>
2023-10-21 13:31:52 +0100

    [FEAT] Add api key

--- src/pages/ClarityConvert.tsx
+++ src/pages/ClarityConvert.tsx
@@ -4,10 +4,13 @@ import { useParams } from "react-router-dom";
 
 function ClarityConvert() {
   let { clarityAddress } = useParams();
+  const apikey = process.env.REACT_APP_OPENAI_API_KEY;
+
   return (
     <div>
       <h1 className="">Welcome to my website!</h1>
       <h2>{clarityAddress}</h2>
+      <h2>hey:{apikey}</h2>
       <Button>Click me</Button>
     </div>
   );
