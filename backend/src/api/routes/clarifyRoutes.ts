fff7d7550418719cb1fd381a6df25d2712fd2e8f
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-10-21 22:50:14 +0100

    [FEAT] Convert solidity to clarity + deploy api

--- backend/src/api/routes/clarifyRoutes.ts
+++ backend/src/api/routes/clarifyRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getContractAnalysis, convertToSolidity } from '../controllers/clarifyController';
+import { getContractAnalysis, convertToSolidity, convertToClarity } from '../controllers/clarifyController';
 
 const router = Router();
 
 router.post('/clarify', getContractAnalysis);
 router.post('/convertToSolidity', convertToSolidity)
+// router.post('/deploy', deployClarityContract)
+router.post('/convertToClarity', convertToClarity)
 
 export default router;
