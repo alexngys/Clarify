8eaca966fa88ca183b1ffa64da9582e5a7ede752
arthur-wronski <arthur.wronski.21@ucl.ac.uk>
2023-10-21 13:12:35 +0100

    tabs

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,44 @@
 import React from "react";
-import { Button } from "flowbite-react";
+import { Tabs } from "flowbite-react";
+
 function Home() {
   return (
-    <div>
-      <h1 className="">Welcome to my website!</h1>
-      <Button>Click me</Button>
+    <div className="flex flex-col items-center justify-center h-screen">
+      {/* Large Bold Centered Title */}
+      <h1 className="text-4xl font-bold mb-8">Clarify</h1>
+
+      <Tabs.Group aria-label="Clarify tabs" style="default">
+        {/* Clarity Tab */}
+        <Tabs.Item
+          active
+          title="Clarity"
+        >
+          <p>
+            This is
+            <span className="font-medium text-gray-800 dark:text-white">
+              Clarity tab's associated content
+            </span>
+            .
+            Clicking the other tab will toggle the visibility of this one. The tab JavaScript swaps classes to
+            control the content visibility and styling.
+          </p>
+        </Tabs.Item>
+
+        {/* Solidity Tab */}
+        <Tabs.Item
+          title="Solidity"
+        >
+          <p>
+            This is
+            <span className="font-medium text-gray-800 dark:text-white">
+              Solidity tab's associated content
+            </span>
+            .
+            Clicking the other tab will toggle the visibility of this one. The tab JavaScript swaps classes to
+            control the content visibility and styling.
+          </p>
+        </Tabs.Item>
+      </Tabs.Group>
     </div>
   );
 }
